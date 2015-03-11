795fc7dd412de304a11b80c072d8fc97d36c4ec8
21011996 <21011996@mail.ru>
2015-03-11 22:07:46 +0300

    12412414

--- implementor/src/ru/ifmo/ctddev/kachalskiy/implementor/Implementor.java
+++ implementor/src/ru/ifmo/ctddev/kachalskiy/implementor/Implementor.java
@@ -11,11 +11,6 @@ import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Ilya on 11.03.2015.
- */
-
-
 public class Implementor implements Impler {
 
     private StringBuilder sb;
@@ -29,7 +24,7 @@ public class Implementor implements Impler {
             Implementor implementor = new Implementor();
             implementor.implement(Class.forName(args[0]), new File(args[1]));
         } catch (Exception e) {
-            System.err.println(e.toString());
+        	System.err.println(e.toString());
         }
     }
     public void implement(Class<?> token, File root) throws ImplerException {
@@ -52,7 +47,7 @@ public class Implementor implements Impler {
             System.out.println("Input/Output error");
         }
     }
-
+    
     private void printClass(Class<?> token) throws ImplerException {
         sb.append("package " + token.getPackage().getName() + ";\n\n");
         sb.append("public class " + token.getSimpleName() + "Impl ");
@@ -74,7 +69,7 @@ public class Implementor implements Impler {
         }
         sb.append("\n" + "    " + "@Override");
         modifiers ^= Modifier.ABSTRACT;
-
+        
         if (Modifier.isTransient(modifiers)) {
             modifiers ^= Modifier.TRANSIENT;
         }
@@ -92,7 +87,7 @@ public class Implementor implements Impler {
             return "null";
         } else {
             if ("char".equals(returnValue.getSimpleName())) {
-                return "''";
+                return "'a'";
             }
             if ("boolean".equals(returnValue.getSimpleName())) {
                 return "true";
@@ -106,30 +101,66 @@ public class Implementor implements Impler {
     private String getArgs(Method method) {
         StringBuilder ans = new StringBuilder();
         Class<?>[] parameters = method.getParameterTypes();
-        int count = parameters.length-1;
-        for (int i=0; i<count; i++) {
-            ans.append(parameters[i].getCanonicalName() + " args" + i + ", ");
-        }
-        if (count>-1){
-            ans.append(parameters[count].getCanonicalName() + " args" + count);
+        for (int i = 0; i < parameters.length; i++) {
+            if (i == parameters.length - 1) {
+                ans.append(parameters[i].getCanonicalName() + " args" + i);
+            } else {
+                ans.append(parameters[i].getCanonicalName() + " args" + i + ", ");
+            }
         }
         return ans.toString();
     }
-    @SuppressWarnings("rawtypes")
     private void collectAllMethods(Class<?> token) {
+
         if (token == null) {
             return;
         }
+
         addAllMethods(token);
+
         Class[] interfaces = token.getInterfaces();
         for (Class i : interfaces) {
             collectAllMethods(i);
         }
+
+        collectAllMethods(token.getSuperclass());
+
     }
+
     private void addAllMethods(Class<?> token) {
+
         Method[] methods = token.getDeclaredMethods();
+
         for (Method method : methods) {
-            allMethods.add(method);
+
+            boolean flag = false;
+            for (int i = 0; i < allMethods.size(); i++) {
+                if (equalMethod(method, allMethods.get(i))) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag == false) {
+                allMethods.add(method);
+            }
         }
+    }
+
+    private boolean equalMethod(Method a, Method b) {
+
+        if (!a.getName().equals(b.getName())) {
+            return false;
+        }
+        Class[] paramTypesA = a.getParameterTypes();
+        Class[] paramTypesB = b.getParameterTypes();
+        if (paramTypesA.length != paramTypesB.length) {
+            return false;
+        }
+        for (int i = 0; i < paramTypesA.length; i++) {
+            if (!(paramTypesA[i].getCanonicalName()).equals(paramTypesB[i].getCanonicalName())) {
+                return false;
+            }
+        }
+        return true;
     }
 }
