8050f628d28d804992d1bc88eb1247334f9862d2
21011996 <21011996@mail.ru>
2015-03-18 22:29:58 +0300

    123

---
+++ implementor/src/ru/ifmo/ctddev/kachalskiy/implementor/Test.java
@@ -0,0 +1,8 @@
+package ru.ifmo.ctddev.kachalskiy.implementor;
+
+import java.io.*;
+
+public interface Test{
+    int calculate(int a, double b, char c);
+
+}
---
+++ implementor/test/ru/ifmo/ctddev/kachalskiy/implementor/TestImpl.java
@@ -0,0 +1,8 @@
+package ru.ifmo.ctddev.kachalskiy.implementor;
+
+public class TestImpl implements ru.ifmo.ctddev.kachalskiy.implementor.Test {
+
+    @Override
+    public int calculate(int args0, double args1, char args2) { return 0; }
+
+}
